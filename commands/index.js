50ac10fbfd2171420aaa1d18b6f74408920a3340
Daniel Balfour <stormbytes@gmail.com>
2016-09-30 20:01:37 -0400

    added changes from Alexander's example

--- commands/index.js
+++ commands/index.js
@@ -1,24 +1,73 @@
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function commandLoader(program) {
-	'use strict';
+// The code below iterate files inside current directory and calls 'require' for each of them
+// (excluding index.js - this file). Result of 'require' it assigns to returning object,
+// each property of which is holding corresponding `require-d module object:
+//
+// {
+//   bye: function() { <function-exported-by-bye-js> },
+//   hello: { <function-exported-by-hello-js> }
+// }
+//
 
+/*
+ This is equivalent to following lines (in case you have only two constant commands):
+ */
+// module.exports = function(program) {
+//   return {
+//     bye:   require('./bye')(program),
+//     hello: require('./hello')(program)
+//   };
+// };
+
+/*
+ Same result may be achieved by using npm module like 'require-dir-all' like that:
+ */
+// module.exports = function(program) {
+//   require('require-dir-all')('.', {
+//       map: function(reqModule) {
+//         reqModule.exports = reqModule.exports( program ); // call exported functions for each module, passing
+//       }
+//     }
+//   );
+// };
+/*
+ You can even do this in one line, but it will require couple of changes in '../index.js':
+ var modules = require('require-dir-all')('.');
+ */
+
+module.exports = function commandLoader(program) {
+
+	// 1. create obj to store commands
 	var commands = {};
+
+	// 2. path/to/commands/dir
 	var loadPath = path.dirname(__filename);
 
-	// Loop though command files
-	fs.readdirSync(loadPath).filter(function (filename) {
+	// 3. retrieve all files in /commands 
+	var modules = fs.readdirSync(loadPath);
+
+	// 4. filter modules array (files ending in .js && !== index.js)
+	var filtered_modules = modules.filter(function (filename) {
 		return (/\.js$/.test(filename) && filename !== 'index.js');
-	}).forEach(function (filename) {
+	});
+
+	// 5. iterates filtered_modules array to build up 'commands' obj
+	filtered_modules.forEach(function (filename) {
+
+		// 5-a. command name = module file name
 		var name = filename.substr(0, filename.lastIndexOf('.'));
 
-		// Require command
+		// 5-b. command definition = require-d module
 		var command = require(path.join(loadPath, filename));
 
-		// Initialize command
+		// 5-c. Initialize command
 		commands[name] = command(program);
 	});
 
+	// 6. output
 	return commands;
 };
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-var program = require ('commander');
-var commands = require ('./commands')(program);
+'use strict';
+
+var fs = require('fs');
+var program = require('commander');
+var commands = require('./commands')(program);
+
+program
+  .usage('<command> [options]')
+
+program.on('*', function() {
+  console.log('Unknown Command: ' + program.args.join(' '));
+  program.help();
+});
+
+// Process Commands
+program.parse(process.argv);
 
